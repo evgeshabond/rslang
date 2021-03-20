46f572a163bcc539a743428a7e506d892fa58132
Gaziz Tuleukulov <gaziz.cyprus@gmail.com>
2021-03-20 19:17:40 +0300

    feat: creat main page component

--- src/pages/Main-page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const MainPage: React.FC = () => (
-  <>
-    <div>Main Page</div>
-  </>
-);
-
-export default MainPage;
---
+++ src/pages/main-page/main-page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage: React.FC = () => {
+  console.log('main');
+  return (
+    <div>
+      <div>main</div>
+    </div>
+  );
+};
+
+export { MainPage };
