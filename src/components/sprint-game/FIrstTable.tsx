a9a69d9454a498a8a19995b6e74cea8d48c5e029
juliememe <julie_meme@mail.ru>
2021-03-23 21:51:39 +0400

    feat: add component

--- src/components/sprint-game/FIrstTable.tsx
+++ src/components/sprint-game/FIrstTable.tsx
@@ -4,12 +4,11 @@ import styles from './sprint-game.module.css';
 const FirstTable: React.FC = () => (
   <div className={styles.first__table}>
     <div className={styles.game__title}>
-      <h2>СПРИНТ</h2>
+      {/* <h2>СПРИНТ</h2> и тут тоже оидн компнент для двух игр только название другое */}
       <p className={styles.subtitle}>
-        Это тренировка для повторения заученных слов из вашего словаря. выберите
-        соответствует ли перевод предложенному слову.
+       {/* text={text} один компонент для двух игр */}
       </p>
-      <button type='button'>PLAY</button>
+      {/* <button type='button'>PLAY</button> здесь аналогично */}
     </div>
   </div>
 );
--- src/components/sprint-game/SprintGame.tsx
+++ src/components/sprint-game/SprintGame.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from './sprint-game.module.css';
 import FirstTable from './FIrstTable';
 
-const SprintGame: React.FC = () => (
+const SprintGame: React.FC = () => {
+
+    const [timer, setTimer] = useState(5);
+
+
+    useEffect(() =>{
+      timer > 0 && setTimeout(() => setTimer( timer -1), 1000);
+    }, [timer]);
+ 
+const startGame = () => {
+    return( <div className={styles.timer}>
+        
+
+    </div>)
+}
+
+ return(
   <div className={styles.sprint__game}>
     <FirstTable />
   </div>
-);
+ )
+};
 
 export default SprintGame;
