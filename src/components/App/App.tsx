706f8de5de950a39d0c613f0a0d2b279947b7fe3
Roman Kohutyak <romankohutyak@gmail.com>
2021-03-26 11:35:15 +0200

    fix: change browserRouter to hashRouter for deploy

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import GamePage from '../../pages/game-page/Game-page';
 import { MainPage } from '../../pages/main-page/main-page';
 import WordsList from '../word-list/words-list';
