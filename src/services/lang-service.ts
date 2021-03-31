ff5ed656f2b4924a3969396cbf515812da415909
Gaziz Tuleukulov <gaziz.cyprus@gmail.com>
2021-03-31 19:56:09 +0300

    feat: add action and reducer for fetch user words

--- src/services/lang-service.ts
+++ src/services/lang-service.ts
@@ -1,22 +1,22 @@
-import { mainPath } from '../utils/constants';
+import { serverUrl } from '../utils/constants';
 
 export default class LangService {
-  langUrl: string;
-
-  constructor() {
-    this.langUrl = mainPath.langUrl;
-  }
-
-  async getWordList(listProps: { page: number; group: number }) {
+  getWordList = async (listProps: { page: number; group: number }) => {
     const { page, group } = listProps;
-    const res = await fetch(
-      `${this.langUrl}words?page=${page}&group=${group}`,
-      {
-        method: 'GET',
-        headers: { 'content-type': 'application/json' },
-      }
-    );
+    const res = await fetch(`${serverUrl}words?page=${page}&group=${group}`, {
+      method: 'GET',
+      headers: { 'content-type': 'application/json' },
+    });
     const data = await res.json();
     return data;
-  }
+  };
+
+  getWord = async (wordId: string) => {
+    const res = await fetch(`${serverUrl}words/:${wordId}`, {
+      method: 'GET',
+      headers: { 'content-type': 'application/json' },
+    });
+    const data = await res.json();
+    return data;
+  };
 }
