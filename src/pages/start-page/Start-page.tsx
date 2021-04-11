5ec68321c41156f5411a333d2bd0a6187c69b438
Roman Kohutyak <romankohutyak@gmail.com>
2021-04-11 17:55:34 +0300

    fix: scroll inside block

--- src/pages/start-page/Start-page.tsx
+++ src/pages/start-page/Start-page.tsx
@@ -11,44 +11,46 @@ import { ReactComponent as Steps } from '../../assets/images/steps.svg';
 
 export const StartPage: React.FC = () => (
   <div className={styles.start}>
-    <h1 className={styles.title}>
-      Учите новые слова легко с приложением Smart Cat!
-    </h1>
-    <Link className={styles.paw} to={mainPath.main}>
-      <Paw className={styles.paw__icon} width={30} height={30} />
-    </Link>
-    <h2 className={styles['sub-title']}>Начать</h2>
-    <ul className={styles.list}>
-      <li className={styles.list__item}>
-        <Base />
-        <span className={styles.list__text}>
-          Большая база слов 3600 разбитых по уровням
-        </span>
-      </li>
-      <li className={styles.list__item}>
-        <MiniGames />
-        <span className={styles.list__text}>
-          Запоминай слова играя в мини-игры
-        </span>
-      </li>
-      <li className={styles.list__item}>
-        <Darts />
-        <span className={styles.list__text}>
-          Определи свой уровень знания языка
-        </span>
-      </li>
-      <li className={styles.list__item}>
-        <SettingsIcon />
-        <span className={styles.list__text}>Настрой приложение для себя</span>
-      </li>
-      <li className={styles.list__item}>
-        <Steps />
-        <span className={styles.list__text}>Отслеживай свои прогресс</span>
-      </li>
-    </ul>
-    <h2 className={styles['sub-title']}>
-      Ознакомься с возможностями приложения подробнее
-    </h2>
-    <div className={styles.video}>Video...</div>
+    <div className={styles.start__wrapper}>
+      <h1 className={styles.title}>
+        Учите новые слова легко с приложением Smart Cat!
+      </h1>
+      <Link className={styles.paw} to={mainPath.main}>
+        <Paw className={styles.paw__icon} width={30} height={30} />
+      </Link>
+      <h2 className={styles['sub-title']}>Начать</h2>
+      <ul className={styles.list}>
+        <li className={styles.list__item}>
+          <Base />
+          <span className={styles.list__text}>
+            Большая база слов 3600 разбитых по уровням
+          </span>
+        </li>
+        <li className={styles.list__item}>
+          <MiniGames />
+          <span className={styles.list__text}>
+            Запоминай слова играя в мини-игры
+          </span>
+        </li>
+        <li className={styles.list__item}>
+          <Darts />
+          <span className={styles.list__text}>
+            Определи свой уровень знания языка
+          </span>
+        </li>
+        <li className={styles.list__item}>
+          <SettingsIcon />
+          <span className={styles.list__text}>Настрой приложение для себя</span>
+        </li>
+        <li className={styles.list__item}>
+          <Steps />
+          <span className={styles.list__text}>Отслеживай свои прогресс</span>
+        </li>
+      </ul>
+      <h2 className={styles['sub-title']}>
+        Ознакомься с возможностями приложения подробнее
+      </h2>
+      <div className={styles.video}>Video...</div>
+    </div>
   </div>
 );
