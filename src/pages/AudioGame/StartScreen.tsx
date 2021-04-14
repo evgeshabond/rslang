ada4dc7763f502f6812201a6c2ca1b7d85c1b27c
Roman Kohutyak <romankohutyak@gmail.com>
2021-04-14 21:48:24 +0300

    fix: audigame and savana styles

--- src/pages/AudioGame/StartScreen.tsx
+++ src/pages/AudioGame/StartScreen.tsx
@@ -6,7 +6,17 @@ import { useFullScreenHandle } from 'react-full-screen';
 import { RootStateType } from '../../reducer/root-reducer';
 import styles from './AudioGame.module.css';
 import { PlayButton } from '../../components/button-icons/playBig-button/playBig-button';
-import { audioGameStart, isAnswerSelected, currentPlayWords, isFullScreen, stepCounter, clearWords, isShowResults, setLearnWords, wordUserAnswer } from '../../actions/audioGame-actions';
+import {
+  audioGameStart,
+  isAnswerSelected,
+  currentPlayWords,
+  isFullScreen,
+  stepCounter,
+  clearWords,
+  isShowResults,
+  setLearnWords,
+  wordUserAnswer,
+} from '../../actions/audioGame-actions';
 import ControlledSelect from '../../components/ControlledSelect/ControlledSelect';
 import { shuffle } from '../../utils/shuffle';
 import { mainPath } from '../../utils/constants';
@@ -17,67 +27,81 @@ import { clearAllCount } from '../../actions/game-result-actions';
 const StartScreen: React.FC = () => {
   const dispatch = useDispatch();
 
-  const isPlaying = useSelector((state: RootStateType) =>
-    state.audioGameState.audioGameStart);
-  const wordList = useSelector((state: RootStateType) =>
-    state.wordState.currentWordList);
-  const rightWord = useSelector((state: RootStateType) =>
-    state.audioGameState.wordRight);
-  const isResults = useSelector((state: RootStateType) =>
-    state.audioGameState.isShowResults);
-  const listLearnWords = useSelector((state: RootStateType) =>
-    state.audioGameState.listLearnWords);
-  const userAnswer = useSelector((state: RootStateType) =>
-    state.audioGameState.wordUserAnswer);
+  const isPlaying = useSelector(
+    (state: RootStateType) => state.audioGameState.audioGameStart
+  );
+  const wordList = useSelector(
+    (state: RootStateType) => state.wordState.currentWordList
+  );
+  const rightWord = useSelector(
+    (state: RootStateType) => state.audioGameState.wordRight
+  );
+  const isResults = useSelector(
+    (state: RootStateType) => state.audioGameState.isShowResults
+  );
+  const listLearnWords = useSelector(
+    (state: RootStateType) => state.audioGameState.listLearnWords
+  );
+  const userAnswer = useSelector(
+    (state: RootStateType) => state.audioGameState.wordUserAnswer
+  );
   const isLevelVisible = useSelector(
     (state: RootStateType) => state.menuState.isLevelVisible
   );
 
-
-  const [play] = useSound(`${mainPath.langUrl}${rightWord.audio}`, { interrupt: true });
+  const [play] = useSound(`${mainPath.langUrl}${rightWord.audio}`, {
+    interrupt: true,
+  });
 
   useEffect(() => {
     playSoundWord();
-  }, [play])
-
+  }, [play]);
 
   const playSoundWord = () => {
-    if ((isPlaying && Object.keys(rightWord).length > 0)) {
-      play()
+    if (isPlaying && Object.keys(rightWord).length > 0) {
+      play();
     }
-  }
+  };
 
   const startGame = () => {
     dispatch(clearAllCount());
-    dispatch(isShowResults(false))
+    dispatch(isShowResults(false));
     dispatch(isAnswerSelected(false));
     dispatch(stepCounter(0));
     dispatch(clearWords());
     if (wordList === undefined) {
       return;
     }
-    const currentPlayList = shuffle(wordList)
-      .filter((item: Object, index: number) => index < 5);
+    const currentPlayList = shuffle(wordList).filter(
+      (item: Object, index: number) => index < 5
+    );
     dispatch(currentPlayWords(currentPlayList));
-  }
+  };
 
   return (
     <div className={styles.game__content}>
-      {   isResults ? (
+      {isResults ? (
         <Results />
       ) : (
-        <div>
+        <div className={styles.game__inner}>
           <div className={styles.game__title}>Аудиовызов</div>
-          <div className={styles.game__decription}>Тренировка улучшает
-          восприятие английской речи на слух.
-          Выберите из предложенных вариантов ответа правильный перевод слова,
-    которое услышите</div>
-          < PlayButton buttonClick={() => { dispatch(audioGameStart(true)); startGame(); }} />
+          <div className={styles.game__decription}>
+            Тренировка улучшает восприятие английской речи на слух. Выберите из
+            предложенных вариантов ответа правильный перевод слова, которое
+            услышите
+          </div>
+          <PlayButton
+            buttonClick={() => {
+              dispatch(audioGameStart(true));
+              startGame();
+            }}
+          />
           {isLevelVisible ? <ControlledSelect /> : null}
-        </div>)}
+        </div>
+      )}
 
       <CatAudio className={styles.cat__image} />
-    </div >
-  )
-}
+    </div>
+  );
+};
 export default StartScreen;
